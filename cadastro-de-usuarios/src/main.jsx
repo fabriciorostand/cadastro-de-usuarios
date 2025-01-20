ccc3b6cf4c0b000e6927163808aa11f71fe29f6c
Fabricio Rostand Morais <fabriciorostand2000@gmail.com>
2025-01-20 00:22:01 -0300

    refactor: revert HashRouter to BrowserRouter to fix routing issues

--- cadastro-de-usuarios/src/main.jsx
+++ cadastro-de-usuarios/src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import './index.css'
 import Login from './pages/Login/login'
 import Welcome from './pages/Welcome/welcome'
 import Register from './pages/Register/register'
 
+const basename = "/cadastro-de-usuarios"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <Router basename={basename}>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path='/login' element={<Login />} />
